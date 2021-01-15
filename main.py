25cd7c069db61077a3a211fb7318916f2deb3245
Trent S <tjschweitzer@gmail.com>
2021-01-15 16:14:57 -0700

    Inital game code works

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy
+
+
+
+        # Course topics
+    # Todo: Methods of manipulating Python numpy arrays using examples from complexity: Game of Life, Diffusion Limited Aggregation.
+    # Todo: The Ising Model
+    # Todo: The Metropolis-Hastings algorithm or the basis of Markov Chain Monte Carlo (MCMC) methods.
+    # Todo: The motion of projectiles.
+    # Todo: Ordinary differential equations for 1,2, and N degrees of freedom.
+    # Todo: Optimization with the simplex and BFGS algorithms.
+    # Todo: The three-body problem.
+    # Todo: The Susceptible, Infected, Recovered (SIR) model for disease propagation.
+    # Todo: The Kalman filter.
+    # Todo: Finite difference approximations to partial differential equations.
+    # Todo: Numerical method of lines.
+
+
+#
+
+if __name__ == '__main__':
+    pass
