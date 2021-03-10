a8ad7729293ace5c8b764c493626264fd29a73ec
Trent S <tjschweitzer@gmail.com>
2021-03-09 17:37:59 -0700

    More Bullet stuff

---
+++ GradProject/scratch.py
@@ -0,0 +1,54 @@
+from sympy import symbols
+from sympy.physics.mechanics import *
+
+q1, q2 = dynamicsymbols('q1 q2')
+q1d, q2d = dynamicsymbols('q1 q2', 1)
+u1, u2 = dynamicsymbols('u1 u2')
+u1d, u2d = dynamicsymbols('u1 u2', 1)
+l, m, g = symbols('l m g')
+
+N = ReferenceFrame('N')
+A = N.orientnew('A', 'Axis', [q1, N.z])
+B = N.orientnew('B', 'Axis', [q2, N.z])
+
+A.set_ang_vel(N, u1 * N.z)
+B.set_ang_vel(N, u2 * N.z)
+
+O = Point('O')
+P = O.locatenew('P', l * A.x)
+R = P.locatenew('R', l * B.x)
+
+O.set_vel(N, 0)
+P.v2pt_theory(O, N, A)
+R.v2pt_theory(P, N, B)
+
+ParP = Particle('ParP', P, m)
+ParR = Particle('ParR', R, m)
+
+kd = [q1d - u1, q2d - u2]
+FL = [(P, m * g * N.x), (R, m * g * N.x)]
+BL = [ParP, ParR]
+
+KM = KanesMethod(N, q_ind=[q1, q2], u_ind=[u1, u2], kd_eqs=kd)
+
+(fr, frstar) = KM.kanes_equations(FL, BL)
+kdd = KM.kindiffdict()
+mm = KM.mass_matrix_full
+fo = KM.forcing_full
+qudots = mm.inv() * fo
+qudots = qudots.subs(kdd)
+qudots.simplify()
+mechanics_printing()
+mprint(qudots)
+
+co = code_output(KM, "outfile")
+co.get_parameters()
+# output should be [m, g, l]
+co.give_parameters([1, 9.81, 1])
+co.get_initialconditions()
+# output should be [q1, q2, u1, u2]
+co.give_initialconditions([.1, .2, 0, 0])
+co.give_time_int([0, 10, 1000])
+
+co.write_settings_file()
+co.write_rhs_file('SciPy')
